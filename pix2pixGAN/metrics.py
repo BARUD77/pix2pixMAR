08586519ab1c5513b50e62e7fadefd219b2fc46e
Hermon B. Teklesenbet <hotelbravo30@gmail.com>
2024-10-26 17:03:16 +0400

    Created metrics.py file

---
+++ pix2pixGAN/metrics.py
@@ -0,0 +1,17 @@
+from skimage.metrics import structural_similarity as ssim
+import math
+import numpy as np
+
+# SSIM and PSNR calculation functions
+def calculate_ssim(img1, img2):
+    img1 = img1.squeeze().cpu().numpy()
+    img2 = img2.squeeze().cpu().numpy()
+    return ssim(img1, img2, data_range=img2.max() - img2.min())
+
+def calculate_psnr(img1, img2):
+    mse = torch.mean((img1 - img2) ** 2).item()
+    if mse == 0:
+        return 100  # If the images are identical
+    max_pixel = 1.0  # Images are normalized between -1 and 1
+    psnr = 20 * math.log10(max_pixel / math.sqrt(mse))
+    return psnr
